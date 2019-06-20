058e03cc9392b211e1eafcf7350622558d14cca1
oluwatobi shokunbi <ayoshokz@gmail.com>
2019-06-20 10:21:25 +0100

    changed landing page message

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Host On Heroku
                 </div>
 
                 <div class="links">
