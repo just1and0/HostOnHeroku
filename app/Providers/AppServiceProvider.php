44278a8be040107fe8404bccf13ec958ee112abd
Oluwatobi Shokunbi <ayoshokz@gmail.com>
2019-06-26 10:38:01 +0100

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,28 +1,24 @@
 <?php
-
 namespace App\Providers;
-
 use Illuminate\Support\ServiceProvider;
-
-class AppServiceProvider extends ServiceProvider
-{
-    /**
-     * Register any application services.
-     *
-     * @return void
-     */
-    public function register()
-    {
-        //
-    }
-
-    /**
-     * Bootstrap any application services.
-     *
-     * @return void
-     */
-    public function boot()
-    {
-        //
-    }
+//import this
+use Illuminate\Support\Facades\Schema;
+class AppServiceProvider extends ServiceProvider{
+  /**
+  * Register any application services.
+  *
+  * @return void
+  */
+  public function register(){
+     //add this
+     Schema::defaultStringLength(191);
+  }
+  /**
+  * Bootstrap any application services.
+  *
+  * @return void
+  */
+   public function boot(){
+      //
+   }
 }
